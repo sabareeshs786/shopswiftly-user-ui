4efb31688486c5e7895cf8fc7304e5b203e0be27
Hackovator <ssabareesh646@gmail.com>
2023-12-16 10:28:27 +0530

    Update the layout of MobileContainer

--- src/components/MobileContainer.js
+++ src/components/MobileContainer.js
@@ -2,20 +2,22 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import '../css/main.css';
 import '../css/cateitems.css';
+import ItemImage from './generic/ItemImage';
 
 function MobileContainer() {
   const [data, setData] = useState(null);
+  const [isLoading, setLoading] = useState(true);
   useEffect(() => {
     const fetchData = async () => {
       try {
         const queryParams = {preview: true, brand: "oppo"}
         const response = await axios.get('http://localhost:3501/items/mobiles', {params: queryParams});
         console.log(response);
-        setData(response.data[0].name);
+        setData(response.data);
       } catch (error) {
         console.log(error);
       } finally {
-
+        setLoading(false);
       }
     };
     fetchData();
@@ -23,13 +25,37 @@ function MobileContainer() {
 
   return (
     <div className="main-container">
-        <div className="flex-container">
-          <div className="flex-header">
-            <h2>Apple smartphones</h2>
+        <div className="item-flex-container">
+          <div className="item-flex-header">
+            <h2>{data && data.length > 0 ? data[0].brand.charAt(0).toUpperCase() + data[0].brand.slice(1) : ''} smartphones</h2>
             <button className='btn btn-primary'>View all</button>
           </div>
-          <div className='flex-items'>
-            <div> Name: {data}</div>
+          <div className='item-flex-items'>
+          {data && data.length > 0 && data.map( (imgData) =>
+            <ItemImage key={imgData.imageUrl} filename={imgData.imageUrl} altText={imgData.name} caption={imgData.name} />
+          )}
+          </div>
+        </div>
+        <div className="item-flex-container">
+          <div className="item-flex-header">
+            <h2>{data && data.length > 0 ? data[0].brand.charAt(0).toUpperCase() + data[0].brand.slice(1) : ''} smartphones</h2>
+            <button className='btn btn-primary'>View all</button>
+          </div>
+          <div className='item-flex-items'>
+          {data && data.length > 0 && data.map( (imgData) =>
+            <ItemImage key={imgData.imageUrl} filename={imgData.imageUrl} altText={imgData.name} caption={imgData.name} />
+          )}
+          </div>
+        </div>
+        <div className="item-flex-container">
+          <div className="item-flex-header">
+            <h2>{data && data.length > 0 ? data[0].brand.charAt(0).toUpperCase() + data[0].brand.slice(1) : ''} smartphones</h2>
+            <button className='btn btn-primary'>View all</button>
+          </div>
+          <div className='item-flex-items'>
+          {data && data.length > 0 && data.map( (imgData) =>
+            <ItemImage key={imgData.imageUrl} filename={imgData.imageUrl} altText={imgData.name} caption={imgData.name} />
+          )}
           </div>
         </div>
     </div>
---
+++ src/components/generic/ItemImage.js
@@ -0,0 +1,17 @@
+import React, { useEffect } from 'react';
+import { Link } from "react-router-dom";
+
+function ItemImage({filename, altText, caption}) {
+  const baseUrl = 'http://localhost:3501/uploads/'
+  return (
+    <Link to="#" className='item-image-container'>
+        <img src={baseUrl + filename} alt={altText} />
+        <div className='item-image-caption'>
+            {caption}<br />
+            <span className='price'>&#8377; 45000</span>
+        </div>
+    </Link>
+  )
+}
+
+export default ItemImage;
